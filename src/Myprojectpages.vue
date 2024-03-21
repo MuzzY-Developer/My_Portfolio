91c1f7d6a81a1dd4fa207c80efe566725fc62617
MuzzY-Devolpers <khamet-devolpers@mail.ru>
2024-03-21 05:23:07 +0500

    completed

--- src/Myprojectpages.vue
+++ src/Myprojectpages.vue
@@ -36,6 +36,52 @@
                       <p  class="myprojectpages__container__cards-card-subtitlecompleted">(Есть 1440px, html,scss)</p>
                 </a>
            
+                <a href="/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./RoyalX.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">RoyalX</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitleedite">(В разработке)</p>
+                 </a>
+           
+            
+                <a href="https://max-waysite.netlify.app/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./MaxWay.mp4"></video>
+                      <h4 class="myprojectpages__container__cards-card-title">MaxWay</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitlecompleted">(Есть 1440px,html.css)</p>
+                </a>
+           
+                <a href="https://crayson.netlify.app/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./Crayson.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">Crayson</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitlecompleted">(Есть 1920px,html,css)</p>
+                </a>
+                <a href="https://lineageservers.netlify.app/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./Lineage2.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">Lineage2</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitlecompleted">(Есть 1440px,html,scss)</p>
+                </a>
+                
+                <a href="https://totembosite.netlify.app/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./Totembo.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">Totembo</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitleedite">(В разработке)</p>
+                </a>
+
+                <a href="https://browserssites.netlify.app/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./Browsers.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">Browsers</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitlecompleted">(Есть,html,css)</p>
+                </a>
+
+                <a href="/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted src="./Food.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">Food</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitleedite">(В разработке)</p>
+                </a>
+                <a href="https://webiositefigma.netlify.app/" class="myprojectpages__container__cards-card">
+                      <video class="myprojectpages__container__cards-card-movie" autoplay loop muted  src="./FigmaWebio.mp4" ></video>
+                      <h4 class="myprojectpages__container__cards-card-title">FigmaWebio</h4>
+                      <p  class="myprojectpages__container__cards-card-subtitlecompleted">(Есть 1440px,html,scss,vue)</p>
+                </a>
             
 
             </div>
